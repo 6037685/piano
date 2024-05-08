06375c710d0a592d53e6896ac3ddddfe01a6661e
kishan <joeko@hotmail.com>
2024-05-08 14:53:04 +0200

    bestanden

---
+++ app.js
@@ -0,0 +1,23 @@
+// let cijfers = 0
+
+// while (cijfers < 10) { 
+//     cijfers++
+// }
+
+// for( let i = 0; i < 11; i++){
+//     console.log(i)
+//     document.body.innerHTML += i + "<br>";
+// }
+
+function woord(auto, aantal) {
+    document.body.innerHTML += auto;
+    for(let i = 1; i <= aantal; i++){
+        document.body.innerHTML += "<br>";
+        document.body.innerHTML += "rajnish";
+        document.body.style.fontSize += "100px"
+    }
+}
+
+woord("rajnish", 100);
+
+
